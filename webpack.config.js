6e91956bdad1dc1b3598d0038dd0075c7ff6c669
ajoyvmon <ajoyvmon@gmail.com>
2022-03-19 17:04:01 -0700

    set up frontend file tree and static pages routes

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path'); // require a module that has methods to get us a directory
+
+module.exports = {
+  context: __dirname,
+  entry: './frontend/mooguri_entry.jsx', // specify where does webpack look?
+  output: {
+    path: path.resolve(__dirname, "app", "assets", "javascripts"),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env', '@babel/react']
+          }
+        },
+      }
+    ]
+  },
+  devtool: 'source-map', // creates a bundle.js.map - without this chrome would only tell you where your errors are in the bundle file
+  resolve: {
+    extensions: [".js", ".jsx", "*"] // import a file - import something from 'something'
+  }
+};
