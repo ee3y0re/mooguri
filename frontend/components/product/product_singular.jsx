572073aad582cd1c41f08a0c10e9485f2af8d6e3
ajoyvmon <ajoyvmon@gmail.com>
2022-03-28 21:21:26 -0700

    products description

--- frontend/components/product/product_singular.jsx
+++ frontend/components/product/product_singular.jsx
@@ -30,41 +30,65 @@ class ProductSingular extends React.Component {
           </div>
           <div className="show-buy-quick">
             <div className="show-seller">
-              <div className="show-seller-name">Abby</div>
-              <div className="show-seller-info">1 sale, 5 stars</div>              
+              <h3 className="product-info-subtitle1">Seller Id: {product.sellerId}</h3>
+              {/* <h4 className="product-info-subtitle2">1 sale, ??? stars</h4>               */}
+            </div>
+            <div id="no-extra-space">
+              <div className="show-buy-title-lines">
+                <h1>{product.productName}</h1>
+              </div>
+              <div className="show-price-avail">
+                <div className="show-price">${product.price}</div>
+                <div className="show-avail">In Stock</div>
+              </div>              
             </div>
 
-            <div className="show-buy-title-lines">
-              <h1>{product.productName}</h1>
-            </div>
-            <div className="show-price-avail">
-              <div className="show-price">${product.price}</div>
-              <div className="show-avail">In Stock</div>
-            </div>
             <button className="dark-button">Add to Cart WIP</button>
             <ul className="shop-item-trends">
               <li className="stock-sell-stats">Other people want this. OR Selling fast! <span className="stock-sell-details">Over # people have this in their carts right now. OR Only 1 left.</span></li>
-              <li className="seller-trend">Seller Fun Fact: The seller can eat bittermelon but cannot eat durian :c</li>
+              <li className="seller-trend">Star Seller (Sometimes)</li>
               <li className="ship-date">Arrives by Due Date of This Project</li>              
             </ul>
             <div className="highlights">
-              <h2 className="highlights-head">Highlights</h2>
-              <ul className="highlights-details">             
+              <h2 className="product-info-head">Highlights</h2>
+              <ul className="product-info-details">  
+                           {/* need to add additional features to schema */}
                 <li>Handmade</li>
-                <li>Height: It's high, trust me :p</li>
-                <li>Width: Why?</li>
-                <li>Depth: First Search</li>
+                <li>Height: </li>
+                <li>Width: </li>
+                <li>Depth: </li>
+                <li>OR</li>
+                <li>Materials: </li>
+                <li>Capacity: </li>
               </ul>              
             </div>
           </div>
 
 
-          <div className="show-buy-info-full"></div>            
+          <div className="show-buy-info-full">
+            <div className="description-container">
+              <h2 className="product-info-head">Description</h2>
+              <p className="product-info-details">{product.description}</p>
+            </div>
+            <div className="eta">
+              <div className="eta-1">
+                <h2 className="product-info-head">Shipping and Return Policies</h2>
+                <h3 className="product-info-subtitle1">Estimated Arrival</h3>                  
+                <span className="custom-small-title">April 1-5</span>
+              </div>
+              <div className="eta-2">
+                <h2 className="product-info-subtitle1">Returns</h2>
+                <span className="custom-small-title">Accepted</span>       
+              </div>
+            </div>
+            <div ></div>
+          </div>            
           <div className="show-reviews">
             <h2>Reviews Under Construction</h2>
           </div>
         </div>
         
+        {/* move to plash */}
         {/* <div className="show-footer-site-directory">
           <div className="show-shop"></div>
           <div className="show-sell"></div>
@@ -76,21 +100,6 @@ class ProductSingular extends React.Component {
           <div className="show-legality"></div>
         </div> */}
       </div>
-
-
-
-      // // coded skeleton
-      // <div className="product-show">
-      //   <img src={product.photoUrl} alt="" />
-      //   <h1>{product.productName}</h1>
-
-      //   <h2>${product.price}</h2>
-      //   {/* Work in logic for availability */}
-      //   <h3>In Stock</h3>
-      //   <button>Add to Cart</button>
-      //   <h4>Description</h4>
-      //   <p>{product.description}</p>
-      // </div>
     )
   }
 }
