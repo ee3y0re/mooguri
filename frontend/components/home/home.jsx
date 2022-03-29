d9c2a9f8a4270ead5ed5a8b59847352080a304c5
ajoyvmon <ajoyvmon@gmail.com>
2022-03-29 14:29:01 -0700

    positioned top ribbon behind text on home page

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -16,19 +16,13 @@ const Home = () => {
     <div id="home-container">
       {/* after creating a general nav component */}
       {/* after creating a category nav component */}
+      <div id="ribbon"></div>
       <main id="frontpage">
         {/* after creating a bubbles component */}
         <div id="top-banner">
           <h1 id="greeting">Cope with March Madness</h1>
-          <div id="bubble-color-ribbon"></div>
           <div className="bubbles-container">
-            <ul>
-              <li>
-                <a href="">
-                  <p></p>
-                </a>
-              </li>
-            </ul>
+
           </div>
         </div>
         {/* product snapshot */}
