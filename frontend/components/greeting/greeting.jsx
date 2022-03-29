79d3683cec63285d54f2568df29e24cd59a1edb4
ajoyvmon <ajoyvmon@gmail.com>
2022-03-29 10:35:25 -0700

    cleaning up stylesheets file

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -4,7 +4,6 @@ const Greeting = ({ currentUser, logout, openModal }) => {
   const completedLogin = () => {
     return (
       <div className="complete-login">
-        <h1>Welcome back, {`${currentUser.username}`}!</h1>
         <button id="logout-button" onClick={logout}>Logout</button>
       </div>
     );
