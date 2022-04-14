c4bbe30c3e5feaf0b02243d9bfb5761c3d9b67bf
ajoyvmon <ajoyvmon@gmail.com>
2022-04-14 12:13:09 -0700

    disable reviews for now

--- frontend/components/review/review_form.jsx
+++ frontend/components/review/review_form.jsx
@@ -1,60 +1,60 @@
-import React from "react";
-import ReviewList from "./review_list";
+// import React from "react";
+// import ReviewList from "./review_list";
 
-class ReviewForm extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log(props)
-    this.state = {
-      body: "",
-      rating: 5,
-      // TODO: REMOVE HARD CODE; delete these line because you need to fix opening the modal to log in when trying to write a comment
-      reviewer_id: 1,
-      // TODO: REMOVE HARD CODE; delete this line because it needs to be dynamic for all products
-      product_id: 1
-    };
-    this.handleSubmit = this.handleSubmit.bind(this)
-  }
+// class ReviewForm extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     console.log(props)
+//     this.state = {
+//       body: "",
+//       rating: 5,
+//       // TODO: REMOVE HARD CODE; delete these line because you need to fix opening the modal to log in when trying to write a comment
+//       reviewer_id: 1,
+//       // TODO: REMOVE HARD CODE; delete this line because it needs to be dynamic for all products
+//       product_id: 1
+//     };
+//     this.handleSubmit = this.handleSubmit.bind(this)
+//   }
 
-  // TODO: do we really need to name it handleSubmit?
-  handleSubmit(e){
-    // debugger
-    e.preventDefault();
-    this.props.submitAction(this.state).then()
-  }
+//   // TODO: do we really need to name it handleSubmit?
+//   handleSubmit(e){
+//     // debugger
+//     e.preventDefault();
+//     this.props.submitAction(this.state).then()
+//   }
 
-  updateField(field){
-    return (e) => { this.setState( {[ field]: e.target.value } )}
-  }
+//   updateField(field){
+//     return (e) => { this.setState( {[ field]: e.target.value } )}
+//   }
 
-  render() {
+//   render() {
 
-    return (
-      <div>
-        <div>
-          <form onSubmit={this.handleSubmit}>
-            {/* TODO: Review stars */}
-            <label id="review-body">Help others by sharing your feedback</label>
-            <p>What do you like about this? Did it ship on time? Describe your experience with this shop</p>
-            <textarea 
-              id="review-body" 
-              type="text" 
-              value={this.state.body}
-              onChange={this.updateField("body")}/>
+//     return (
+//       <div>
+//         <div>
+//           <form onSubmit={this.handleSubmit}>
+//             {/* TODO: Review stars */}
+//             <label id="review-body">Help others by sharing your feedback</label>
+//             <p>What do you like about this? Did it ship on time? Describe your experience with this shop</p>
+//             <textarea 
+//               id="review-body" 
+//               type="text" 
+//               value={this.state.body}
+//               onChange={this.updateField("body")}/>
             
-            <label>Rating:
-              <input type="text" onChange={this.updateField("rating")}/>
-            </label>
+//             <label>Rating:
+//               <input type="text" onChange={this.updateField("rating")}/>
+//             </label>
 
-            <input type="submit" value="Post Your Review"/>
-          </form>       
-        </div>
+//             <input type="submit" value="Post Your Review"/>
+//           </form>       
+//         </div>
 
-      </div>
+//       </div>
 
 
-    )
-  }
-}
+//     )
+//   }
+// }
 
-export default ReviewForm;
+// export default ReviewForm;
--- frontend/components/review/review_form_container.js
+++ frontend/components/review/review_form_container.js
@@ -1,22 +1,22 @@
-import { connect } from "react-redux";
-import { createReview, fetchReviews } from "../../actions/review_actions";
-import ReviewForm from "./review_form";
+// import { connect } from "react-redux";
+// import { createReview, fetchReviews } from "../../actions/review_actions";
+// import ReviewForm from "./review_form";
 
-const napstablook = (state) => {
-  return {
-    review: {
-      body: "",
-      rating: 5
-    }
-  };
-};
+// const napstablook = (state) => {
+//   return {
+//     review: {
+//       body: "",
+//       rating: 5
+//     }
+//   };
+// };
 
-const blookstanap = (dispatch) => {
-  // debugger
-  return {
-    submitAction: (reviewMagic) => dispatch(createReview(reviewMagic)),
-    updateAction: () => dispatch(fetchReviews())
-  }
-}
+// const blookstanap = (dispatch) => {
+//   // debugger
+//   return {
+//     submitAction: (reviewMagic) => dispatch(createReview(reviewMagic)),
+//     updateAction: () => dispatch(fetchReviews())
+//   }
+// }
 
-export default connect(napstablook,blookstanap)(ReviewForm);
+// export default connect(napstablook,blookstanap)(ReviewForm);
