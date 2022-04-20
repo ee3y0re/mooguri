2fed82be2fb5a5f778f5b08940c69b151a001090
ajoyvmon <ajoyvmon@gmail.com>
2022-04-20 15:48:21 -0700

    add event listener to radio rating buttons to update state

--- frontend/components/review/review_form.jsx
+++ frontend/components/review/review_form.jsx
@@ -12,6 +12,11 @@ class ReviewForm extends React.Component {
   }
 
 
+  update(field){
+    return (e) => {
+      this.setState({ [field]: e.target.value })
+    }
+  }
 
   render() {
     // console.log("review form props, current user", this.props.currentUser.id)
@@ -21,15 +26,15 @@ class ReviewForm extends React.Component {
         <form onSubmit={(e) => handleSubmit(e)}>
           <h2>My review</h2>
           {/* adding name attribute to rating prevents multiple from being selected */}
-          <input name="rating" type="radio" id="rating-5" value='5'/>
+          <input name="rating" type="radio" id="rating-5" value='5' onClick={this.update("rating")}/>
           <label for="rating-5">5</label>
-          <input name="rating" type="radio" id="rating-4" value='4' />
+          <input name="rating" type="radio" id="rating-4" value='4' onClick={this.update("rating")}/>
           <label for="rating-4">4</label>
-          <input name="rating" type="radio" id="rating-3" value='3' />
+          <input name="rating" type="radio" id="rating-3" value='3' onClick={this.update("rating")}/>
           <label for="rating-3">3</label>
-          <input name="rating" type="radio" id="rating-2" value='2' />
+          <input name="rating" type="radio" id="rating-2" value='2' onClick={this.update("rating")}/>
           <label for="rating-2">2</label>
-          <input name="rating" type="radio" id="rating-1" value='1' />
+          <input name="rating" type="radio" id="rating-1" value='1' onClick={this.update("rating")}/>
           <label for="rating-1">1</label>
         </form>
       </div>
