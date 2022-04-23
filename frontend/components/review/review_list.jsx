1ebfa0f50099b7049f57f1e2ae9938fb3fd81796
ajoyvmon <ajoyvmon@gmail.com>
2022-04-23 15:11:15 -0700

    refactor review list to render reviews (finally)

--- frontend/components/review/review_list.jsx
+++ frontend/components/review/review_list.jsx
@@ -13,14 +13,19 @@ class ReviewList extends React.Component {
   render(){
     if (!this.props) { return null }
 
-    const { product, reviews } = this.props;
-    const targetReviewIds = product?.selectedReviews;
-    const reviewsArray = Object.values(reviews)
-    const acquiredReviews = reviewsArray.filter(reviewObj => targetReviewIds?.includes(reviewObj.id));
-    console.log(acquiredReviews[0])
+    const { reviews } = this.props;
+
     return (
       <div>
         <h1>Reviews</h1>
+        {
+          reviews.map((review, idx) => (
+            <div key={idx}>
+              <h2>{review.username}</h2>
+              <p>{review.body}</p>
+            </div>
+          ))
+        }
       </div>
     )
   }
