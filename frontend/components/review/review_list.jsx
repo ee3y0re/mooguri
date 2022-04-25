626c96a5976b4d82a4ec6d345e90ad594f102d1c
ajoyvmon <ajoyvmon@gmail.com>
2022-04-24 20:31:28 -0700

    style reviews

--- frontend/components/review/review_list.jsx
+++ frontend/components/review/review_list.jsx
@@ -12,22 +12,26 @@ class ReviewList extends React.Component {
     return (
       <div>
         <h1 className="review-head">{reviews?.length} reviews</h1>
-        {
-          this.props.currentUser ? <button onClick={
-            () => this.props.openModal("Create Review")
-          } className="dark-button" id="create-review">
-            Create a review</button> : <span>
-              Please sign in to leave a review
-            </span>
-        }
+        <div id="review-action">
+          {
+            this.props.currentUser ? <button onClick={
+              () => this.props.openModal("Create Review")
+            } className="dark-button" id="create-review">
+              Create a review</button> : <span id="review-not-logged-in">
+                Please sign in to leave a review
+              </span>
+          }
+        </div>
         <br />
-        <br />
-        <br />
+
         {
           reviews?.map((review, idx) => (
             <div key={idx}>
-              <h2>{review.username}</h2>
-              <p>{review.body}</p>
+              <div className="review-row">
+                <h2 className="review-author">{review.username}</h2>
+                <p className="review-body">{review.body}</p>
+              </div>
+              <br />
             </div>
           ))
         }
