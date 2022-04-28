9e2b2344ad54977d726bb0f72b279d67f518cce0
ajoyvmon <ajoyvmon@gmail.com>
2022-04-27 21:19:03 -0700

    add review creation date

---
+++ frontend/components/review/edit_review_form.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+class EditReviewForm extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     review : this.props.review //need review from props
+  //   }
+  // }
+
+  render(){
+    return (
+      <div>From the review edit</div>
+    )
+  }
+}
+
+export default EditReviewForm
--- frontend/components/review/review_list.jsx
+++ frontend/components/review/review_list.jsx
@@ -1,15 +1,33 @@
 import React from "react";
-import CreateReviewFormContainer from  "./create_review_form_container"
+import CreateReviewFormContainer from  "./create_review_form_container";
+import EditReviewForm from "./edit_review_form"
 
 class ReviewList extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      editDisplay : false
+    }
+  }
+
+  handleEditClick(){
+    this.setState({ editDisplay : !this.state.editDisplay });
+    // on handle submit edit, need to set edit display as false
   }
 
   render(){
     if (!this.props) { return null }
-
     const { reviews, product } = this.props;
+    const monthsAbbrev = [
+      "Jan", "Feb", "Mar", "Apr", "May", "Jun", 
+      "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+    ];
+    const test = reviews[reviews.length - 1].createdAt;
+    const testNum = new Date(test);
+    console.log(testNum.getMonth())
+    const index = testNum.getMonth();
+    console.log(monthsAbbrev[index] + " " + testNum.getDate() + " " + testNum.getFullYear())
+
     return (
       <div className="review-list-box">
         <br />
@@ -31,27 +49,36 @@ class ReviewList extends React.Component {
               <div key={idx}>
                 <div className="review-row">
                   <h2 className="review-author">{review.username}</h2>
+                  <br />
+                  <h3>
+                    {
+                      monthsAbbrev[new Date(review.createdAt).getMonth()] +
+                      " " + new Date(review.createdAt).getDate() + " " + 
+                      new Date(review.createdAt).getFullYear()
+                    }
+                  </h3>
+                  <br />
                   <p className="review-body">{review.body}</p>
                   {
                     this.props.currentUser?.id === review.reviewerId ?
-                      <div className="review-edit-delete-buttons">
-                        <button 
-                          className="dark-button" 
-                          id="submit-review" 
-                          onClick={() => console.log("work on delete review")}
-                          // link to edit review form component
-                        >
-                          Edit
-                        </button>
-                        <button 
-                          className="dark-button" 
-                          id="submit-review" 
-                          onClick={() => console.log("work on edit review")}
-                        >
-                          Delete
-                        </button>
-                      </div> :
-                      <></>
+                    <div className="review-edit-delete-buttons">
+                      <button 
+                        className="dark-button" 
+                        id="submit-review" 
+                        onClick={() => console.log("work on edit")}
+                      >
+                        Edit
+                      </button>
+
+                      <button 
+                        className="dark-button" 
+                        id="submit-review" 
+                        onClick={() => console.log("work on delete review")}
+                      >
+                        Delete
+                      </button>
+                    </div> :
+                    <></>
                   }
                   <br />
                   <br />
