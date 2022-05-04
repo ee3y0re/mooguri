36e3e142d4f7b3a9f75e7418c0297adb296e98e1
ajoyvmon <ajoyvmon@gmail.com>
2022-05-03 18:35:54 -0700

    add yogurt and reorganize seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,34 +18,6 @@ User.create!([
   { username: "EtsyListings", email: "etsyclone.com", password: "mooguriChild1" }
 ])
 
-milk1_product = Product.create!({
-  product_name: "Milk Jug Charm Miniature Food Jewelry Resin Charms Handmade Jewelry from AllSoCharming", 
-  description: "Milk Jug Charm Miniature Food Jewelry Resin Charms Handmade Jewelry. A super realistic milk jug charm complete with it's own tiny nutrition label! Each charm comes attached to a silver lobster clasp and measures roughly 3/4 inches tall. If you are gifting this charm and would like to leave a short personal message with your order you can do so by adding the message to the 'notes to seller' section during checkout. All of my jewelry comes gift wrapped inside a padded jewelry box and is shipped via USPS First Class Mail inside a bubbled mailer.", 
-  price: 12, 
-  category: "milk", 
-  seller_id: User.fourth.id, 
-  availability: 300,
-})
-
-milk1_pic = URI.open('https://mooguri-dev.s3.us-west-1.amazonaws.com/milk_il_1588xn.3239319059_cwz1.jpeg')
-milk1_product.photo.attach(io: milk1_pic, filename: "/milk_il_1588xn.3239319059_cwz1.jpeg")
-
-# photoVariable = URI.open('Object Url')
-# # variableForProductInstance is an instance of the Product.create! or whatever your making
-# variableForProductInstance.photo.attach(io: photoVariable, filename: "/key")
-
-cheese1_product = Product.create!({
-  product_name: "Cheese Flight from KeystoneFarmsCheese", 
-  description: "Any four 8 oz. blocks of cheese from our Best Sellers (NOTE: if you do not specify which 8 oz. blocks of cheese you would like to be included, we will choose four (2) blocks. Additional charges may occur on variety of cheese(s) selected). Pack of crackers. Do you have a large order of 10 or more gift packs? Learn more about our large quantity gift package options at our Business Gifts pages. Due to the perishable nature of our products, we do not ship over the weekend. Most orders placed Thursday-Sunday will ship the following Monday, unless Saturday delivery is available in your area. We will notify you with a tracking number once your package ships. *All orders will include an insulated box. Sorry, we do not ship to Hawaii or Alaska.", 
-  price: 54.50, 
-  category: "cheese", 
-  seller_id: User.fourth.id, 
-  availability: 300,
-})
-
-cheese1_pic = URI.open('https://mooguri-dev.s3.us-west-1.amazonaws.com/cheese_1_il_794xN.3383346041_1a5j.jpeg')
-cheese1_product.photo.attach(io: cheese1_pic, filename: "/cheese_1_il_794xN.3383346041_1a5j.jpeg")
-
 # product = Product.create!({
 #   product_name:"",
 #   description:"",
@@ -57,6 +29,32 @@ cheese1_product.photo.attach(io: cheese1_pic, filename: "/cheese_1_il_794xN.3383
 
 # pic = URI.open()
 # product.photo.attach(io:, filename:"")
+
+# photoVariable = URI.open('Object Url')
+# # variableForProductInstance is an instance of the Product.create! or whatever your making
+# variableForProductInstance.photo.attach(io: photoVariable, filename: "/key")
+
+milk1_product = Product.create!({
+  product_name: "Milk Jug Charm Miniature Food Jewelry Resin Charms Handmade Jewelry from AllSoCharming", 
+  description: "Milk Jug Charm Miniature Food Jewelry Resin Charms Handmade Jewelry. A super realistic milk jug charm complete with it's own tiny nutrition label! Each charm comes attached to a silver lobster clasp and measures roughly 3/4 inches tall. If you are gifting this charm and would like to leave a short personal message with your order you can do so by adding the message to the 'notes to seller' section during checkout. All of my jewelry comes gift wrapped inside a padded jewelry box and is shipped via USPS First Class Mail inside a bubbled mailer.", 
+  price: 12, 
+  category: "milk", 
+  seller_id: User.fourth.id, 
+  availability: 300,
+})
+milk1_pic = URI.open('https://mooguri-dev.s3.us-west-1.amazonaws.com/milk_il_1588xn.3239319059_cwz1.jpeg')
+milk1_product.photo.attach(io: milk1_pic, filename: "/milk_il_1588xn.3239319059_cwz1.jpeg")
+
+cheese1_product = Product.create!({
+  product_name: "Cheese Flight from KeystoneFarmsCheese", 
+  description: "Any four 8 oz. blocks of cheese from our Best Sellers (NOTE: if you do not specify which 8 oz. blocks of cheese you would like to be included, we will choose four (2) blocks. Additional charges may occur on variety of cheese(s) selected). Pack of crackers. Do you have a large order of 10 or more gift packs? Learn more about our large quantity gift package options at our Business Gifts pages. Due to the perishable nature of our products, we do not ship over the weekend. Most orders placed Thursday-Sunday will ship the following Monday, unless Saturday delivery is available in your area. We will notify you with a tracking number once your package ships. *All orders will include an insulated box. Sorry, we do not ship to Hawaii or Alaska.", 
+  price: 54.50, 
+  category: "cheese", 
+  seller_id: User.fourth.id, 
+  availability: 300,
+})
+cheese1_pic = URI.open('https://mooguri-dev.s3.us-west-1.amazonaws.com/cheese_1_il_794xN.3383346041_1a5j.jpeg')
+cheese1_product.photo.attach(io: cheese1_pic, filename: "/cheese_1_il_794xN.3383346041_1a5j.jpeg")
 
 yogurt1_product = Product.create!({
   product_name:"Heirloom Yogurt Starter Cultures OR Sourdough Starters from PositivelyProbiotic",
@@ -72,9 +70,8 @@ yogurt1_product = Product.create!({
   seller_id: User.fourth.id,
   availability: 300
 })
-
-pic = URI.open()
-product.photo.attach(io:, filename:"")
+yogurt1_pic = URI.open("https://mooguri-dev.s3.us-west-1.amazonaws.com/yogurt_il_794xN.1966709327_46j2.jpg")
+yogurt1_product.photo.attach(io: yogurt1_pic, filename:"/yogurt_il_794xN.1966709327_46j2.jpg")
 
 
 Review.create!([
