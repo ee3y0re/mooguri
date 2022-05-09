a00f2de89bce75440b65200d0939a965b2276799
ajoyvmon <ajoyvmon@gmail.com>
2022-05-09 15:27:21 -0700

    create and test cart api util

---
+++ frontend/util/cart_api_util.js
@@ -0,0 +1,21 @@
+export const fetchUserCartItems = () => { //@cart_items
+  return $.ajax({
+    url: "/api/carts",
+    method: "GET"
+  });
+};
+
+export const addItemtoCart = (cart) => { //@cart
+  return $.ajax({
+    url: "/api/carts",
+    method: "POST",
+    data: { cart }
+  });
+};
+
+export const deleteItemOnCart = (cartItemId) => { //@cart but id in route
+  return $.ajax({
+    url: `/api/carts/${cartItemId}`,
+    method: "DELETE"
+  });
+};
