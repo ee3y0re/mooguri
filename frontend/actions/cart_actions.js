357d12ed37403fad7d1a4f9655fff92244700a7c
ajoyvmon <ajoyvmon@gmail.com>
2022-05-09 15:34:24 -0700

    make cart action constants

---
+++ frontend/actions/cart_actions.js
@@ -0,0 +1,6 @@
+import * as CartApiUtil from "../util/cart_api_util";
+
+// constants to be exported to reducers
+export const RECEIVE_CART_ITEMS = "RECEIVE_CART_ITEMS";
+export const RECEIVE_CART_ITEM = "RECEIVE_CART_ITEM";
+export const DELETE_CART_ITEM = "DELETE_CART_ITEM";
