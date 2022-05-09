98ce0cdcbaa910f3847e661206619cd1a8e7ccdc
ajoyvmon <ajoyvmon@gmail.com>
2022-05-09 15:38:08 -0700

    add regular cart action creators

--- frontend/actions/cart_actions.js
+++ frontend/actions/cart_actions.js
@@ -4,3 +4,25 @@ import * as CartApiUtil from "../util/cart_api_util";
 export const RECEIVE_CART_ITEMS = "RECEIVE_CART_ITEMS";
 export const RECEIVE_CART_ITEM = "RECEIVE_CART_ITEM";
 export const DELETE_CART_ITEM = "DELETE_CART_ITEM";
+
+// regular action creators
+const receiveCartItems = () => {
+  return {
+    type: "RECEIVE_CART_ITEMS",
+    carts
+  };
+};
+
+const receiveCartItem = (cart) => {
+  return {
+    type: "RECEIVE_CART_ITEM",
+    cart
+  };
+};
+
+const deleteCartItem = (cartItemId) => {
+  return {
+    type: "DELETE_CART_ITEM",
+    cartItemId
+  };
+};
