f75197918cc61be0f32a50b9596531d48971a544
ajoyvmon <ajoyvmon@gmail.com>
2022-05-10 16:33:48 -0700

    import Link and create ternary for null session

--- frontend/components/cart/cart_checkout.jsx
+++ frontend/components/cart/cart_checkout.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 import { fetchUserCartItems } from "../../actions/cart_actions";
 import CartCheckoutItem from "./cart_checkout_items";
 
@@ -9,6 +10,9 @@ const CartCheckout = () => {
   const currentCart = useSelector((wholeState) => {
     return wholeState.entities.carts;
   });
+  const activeSession = useSelector((state) => {
+    return state.session.id
+  })
   const dispatch = useDispatch();
 
   /* componentDidMount */
@@ -16,92 +20,62 @@ const CartCheckout = () => {
     dispatch(fetchUserCartItems());
   },[]); //empty array only has useEffect run on mount and unmount
 
+  console.log(activeSession)
+
   return (
     <div className="checkout-main-contain">
       <div className="checkout-flex-box-width-1400px">
         <div className="checkout-single-item">
-          <header className="checkout-title-and-main-redirect">
-            {
-              !currentCart ?
-                <h1>Your cart is empty.</h1> :
+          {
+            !activeSession ?
+            <div className="checkout-null-session">
+              <header className="checkout-title-and-main-redirect">
+                <h1>Your cart is empty.</h1>
+                  <Link to="/search?abbySpeak=">Discover something unique to fill it up</Link>
+                <span>
+                  Mooguri offsets carbon emissions from not actually delivering.
+                </span>
+              </header>
+            </div>:
+            <div>
+              <header className="checkout-title-and-main-redirect">
                 <h1>{Object.keys(currentCart).length} items in your cart</h1>
-            }
-            <button className="checkout-bold-heading">Keep Shopping</button>
-          </header>
-
-
-          {/* <br />
-          <CartCheckoutItem wholeCart={currentCart}/>
-          <br />
-          <br /> */}
-
-
-          <div className="checkout-two-column">
-            <CartCheckoutItem wholeCart={currentCart} />
-            {/* <ul>
-              {
-                cartItems?.map((item, idx) => {
-                  return(
-                    <li 
-                      key={cartIds[idx]} 
-                      className="checkout-products-container"
-                    >
-                      {item.productName}
-                    </li>
-                  )
-                })
-              }
-            </ul> */}
-
-
-            {/* <article className="checkout-products-container">
-              <h2 className="checkout-bold-heading">Seller's name</h2>
-              <div className="checkout-flex-box-product-info">
-                <img id="checkout-prod-temp" src={window.placeholder} alt="placeholder" />
-                <div className="checkout-flexbox-name-space-price">
-                  <div className="checkout-flex-box-prod-mid">
-                    <span>
-                      This is for the product name but I am trying to have the 
-                      title fill up this space because I don't think I will add a 
-                      quantity property :p
-                    </span>
-                    <button>Remove</button>
+                <button className="checkout-bold-heading">
+                  <Link to="/">Keep Shopping</Link>
+                </button>
+              </header>
+              <div className="checkout-two-column">
+                <CartCheckoutItem wholeCart={currentCart} />
+                <aside className="checkout-payment-container">
+                  <div className="checkout-payment-padding">
+                    <h2 className="checkout-bold-heading">How you'll pay</h2>
+                    <ul>
+                      <li>Payment 1: Visa, Master, Amex, and Discover</li>
+                      <li>Payment 2: Paypal</li>
+                      <li>Klarna</li>
+                    </ul>
+                    <div className="price-line">
+                      <span className="checkout-bold-heading">Item(s) total</span>
+                      <span className="price-num">$99.99</span>
+                    </div>
+                    <div className="price-line" id="checkout-discount">
+                      <span className="checkout-bold-heading">Shop discount (THANKU25)</span>
+                      <span className="price-num">-25%</span>
+                    </div>
+                    <div className="price-line">
+                      <span>Subtotal</span>
+                      <span>$99.99</span>
+                    </div>
+                    <div className="price-line" id="checkout-shipping">
+                      <span>Shipping</span>
+                      <span>FREE</span>
+                    </div>
+                    <button id="auth-submit-button">Proceed to checkout</button>
                   </div>
-                  <span className="checkout-bold-heading">$99.99</span>
-                </div>
+                </aside>
               </div>
-            </article> */}
-
-
-
-            <aside className="checkout-payment-container">
-              <div className="checkout-payment-padding">
-                <h2 className="checkout-bold-heading">How you'll pay</h2>
-                <ul>
-                  <li>Payment 1: Visa, Master, Amex, and Discover</li>
-                  <li>Payment 2: Paypal</li>
-                  <li>Klarna</li>
-                </ul>
-                <div className="price-line">
-                  <span className="checkout-bold-heading">Item(s) total</span>
-                  <span className="price-num">$99.99</span>
-                </div>
-                <div className="price-line" id="checkout-discount">
-                  <span className="checkout-bold-heading">Shop discount (THANKU25)</span>
-                  <span className="price-num">-25%</span>
-                </div>
-                <div className="price-line">
-                  <span>Subtotal</span>
-                  <span>$99.99</span>
-                </div>
-                <div className="price-line" id="checkout-shipping">
-                  <span>Shipping</span>
-                  <span>FREE</span>
-                </div>
-                <button id="auth-submit-button">Proceed to checkout</button>
-              </div>
-            </aside>
-          </div>
+            </div>
+          }
         </div>
       </div>
     </div>
