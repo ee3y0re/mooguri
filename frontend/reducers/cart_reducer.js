8b043aac23b51776cab12685ea50fed7f0f98f06
ajoyvmon <ajoyvmon@gmail.com>
2022-05-09 18:05:00 -0700

    create and test cart reducer

---
+++ frontend/reducers/cart_reducer.js
@@ -0,0 +1,25 @@
+import {
+  RECEIVE_CART_ITEMS, 
+  RECEIVE_CART_ITEM, 
+  DELETE_CART_ITEM
+} from "../actions/cart_actions";
+
+const CartsReducer = (state = {}, action) => {
+  Object.freeze(state);
+  let nextState = Object.assign({}, state);
+  switch(action.type) {
+  case RECEIVE_CART_ITEMS:
+    return action.carts;
+  case RECEIVE_CART_ITEM:
+    // nextState[action.cart.id] = action.cart;
+    // return nextState;
+    return action.cart
+  case DELETE_CART_ITEM:
+    delete nextState[action.cartItemId];
+    return nextState;
+  default:
+    return state;
+  }
+}
+
+export default CartsReducer;
