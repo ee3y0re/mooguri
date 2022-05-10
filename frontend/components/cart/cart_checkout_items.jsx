59bee17285333910cf1070b0d8bb6739605b87d4
ajoyvmon <ajoyvmon@gmail.com>
2022-05-10 15:30:01 -0700

    clean console logs

--- frontend/components/cart/cart_checkout_items.jsx
+++ frontend/components/cart/cart_checkout_items.jsx
@@ -3,8 +3,6 @@ import React from "react";
 const CartCheckoutItem = ({wholeCart}) => {
   const cartIds = Object.keys(wholeCart);
   const cartItems = Object.values(wholeCart);
-  console.log("cartIds", cartIds);
-  console.log("cartItems", cartItems);
 
   return (
     <ul className="checkout-products-container">
