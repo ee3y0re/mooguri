329d9f6b24899067c645d1a0796f36fb993ded70
ajoyvmon <ajoyvmon@gmail.com>
2022-05-12 13:47:01 -0700

    establish basic directory component

--- frontend/components/footer/directory.jsx
+++ frontend/components/footer/directory.jsx
@@ -1,2 +1,27 @@
 // will render at home page, product page, and checkout page
 // import into app
+
+import React from "react";
+
+const Directory = () => {
+  return (
+    <ul className="social-links-list">
+      <li className="social-links-list__list-items">
+        <a href="#">
+          placeholder
+        </a>
+      </li>
+      <li className="social-links-list__list-items">
+        <a href="#">placeholder</a>
+      </li>
+      <li className="social-links-list__list-items">
+        <a href="#">placeholder</a>
+      </li>
+      <li className="social-links-list__list-items">
+        <a href="#">placeholder</a>
+      </li>
+    </ul>
+  )
+}
+
+export default Directory;
