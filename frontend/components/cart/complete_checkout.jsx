bf8fb8c27271c41fcbf8956246a44ea6e6ae42e4
ajoyvmon <ajoyvmon@gmail.com>
2022-05-13 20:51:08 -0700

    fix checkout complete border bottom

--- frontend/components/cart/complete_checkout.jsx
+++ frontend/components/cart/complete_checkout.jsx
@@ -3,11 +3,13 @@ import React from "react";
 const CompleteCheckout = () => {
   return (
     <div className="checkout-complete-container">
-      <h1>Thanks for your demo order!</h1>
-      <div className="checkout-carbo-message">You can go through the actual purchasing process on&nbsp;
-        <a href="https://www.etsy.com/" target="_blank">
-          Etsy.com
-        </a>
+      <div className="checkout-complete">
+        <h1>Thanks for your demo order!</h1>
+        <div className="checkout-carbo-message">You can go through the actual purchasing process on&nbsp;
+          <a href="https://www.etsy.com/" target="_blank">
+            Etsy.com
+          </a>
+        </div>
       </div>
     </div>
   )
