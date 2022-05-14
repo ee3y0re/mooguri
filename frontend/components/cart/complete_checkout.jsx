8243452ff5d176a3710aabb4915a49ccea9717d9
ajoyvmon <ajoyvmon@gmail.com>
2022-05-13 20:38:47 -0700

    finish checkout complete component

--- frontend/components/cart/complete_checkout.jsx
+++ frontend/components/cart/complete_checkout.jsx
@@ -2,12 +2,13 @@ import React from "react";
 
 const CompleteCheckout = () => {
   return (
-    <div className="checkout-complete-display-block">
-      Thank you for demo ordering! You can go through the 
-      actual purchasing process on&nbsp;
-      <a href="https://www.etsy.com/" target="_blank">
-        Etsy.com
-      </a>
+    <div className="checkout-complete-container">
+      <h1>Thanks for your demo order!</h1>
+      <div className="checkout-carbo-message">You can go through the actual purchasing process on&nbsp;
+        <a href="https://www.etsy.com/" target="_blank">
+          Etsy.com
+        </a>
+      </div>
     </div>
   )
 };
