85b20519fe71f2b6f41b728ff1074d9359e0c336
ajoyvmon <ajoyvmon@gmail.com>
2022-11-24 19:59:30 -0800

    fix mismatch seeding for collections

--- db/seeds.rb
+++ db/seeds.rb
@@ -1035,19 +1035,40 @@ Category.create!([
   { name: "Yogurt" } #5
 ])
 
-collection_sorted = []
-Product.all.each_slice(6) do |prod_1, prod_2, prod_3, prod_4, prod_5, prod_6| 
-  collection_sorted.push([prod_1, prod_2, prod_3, prod_4, prod_5, prod_6])
-end
-
-Category.all.each_with_index do |category, idx|
-  category_collection = collection_sorted[idx]
-  category_collection.each do |individ_prod|
-    Collection.create! (
-      {
-        product_id: individ_prod.id,
-        category_id: category.id
-      }
-    )
-  end
-end
+Collection.create! ([
+  # cheese
+  { product_id: 1, category_id: 1 },
+  { product_id: 2, category_id: 1 },
+  { product_id: 3, category_id: 1 },
+  { product_id: 4, category_id: 1 },
+  { product_id: 5, category_id: 1 },
+  { product_id: 6, category_id: 1 },
+  # Milk
+  { product_id: 7, category_id: 2 },
+  { product_id: 8, category_id: 2 },
+  { product_id: 9, category_id: 2 },
+  { product_id: 10, category_id: 2 },
+  { product_id: 11, category_id: 2 },
+  { product_id: 12, category_id: 2 },
+  # Lactose-free
+  { product_id: 13, category_id: 3 },
+  { product_id: 14, category_id: 3 },
+  { product_id: 15, category_id: 3 },
+  { product_id: 16, category_id: 3 },
+  { product_id: 17, category_id: 3 },
+  { product_id: 18, category_id: 3 },
+  # Puns
+  { product_id: 19, category_id: 4 },
+  { product_id: 20, category_id: 4 },
+  { product_id: 21, category_id: 4 },
+  { product_id: 22, category_id: 4 },
+  { product_id: 23, category_id: 4 },
+  { product_id: 24, category_id: 4 },
+  # Yogurt
+  { product_id: 25, category_id: 5 },
+  { product_id: 26, category_id: 5 },
+  { product_id: 27, category_id: 5 },
+  { product_id: 28, category_id: 5 },
+  { product_id: 29, category_id: 5 },
+  { product_id: 30, category_id: 5 },
+])
