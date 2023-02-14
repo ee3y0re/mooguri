815ca50b6153a4fc19883d884314702477521892
ajoyvmon <ajoyvmon@gmail.com>
2023-02-13 21:49:33 -0800

    fix category mispelling bug

--- frontend/actions/category_actions.js
+++ frontend/actions/category_actions.js
@@ -4,13 +4,14 @@ import * as CategoryApiUtil from "../util/category_api_util";
 
 // create constants to pass to reducers
 // export constant which equals to string
-export const RECEIVE_CATEGORIES = "RECIEVE_CATEGORIES";
+export const RECEIVE_CATEGORIES = "RECEIVE_CATEGORIES";
 export const RECEIVE_CATEGORY_PRODUCTS = "RECEIVE_CATEGORY_PRODUCTS";
 
 // create regular action creators with defined type
 // function that takes in type of data, 
   // assigns stringified constant to type as key value pair,
   // then just the key with the data, shorted for data: data
+// #2
 const receiveCategories = (categories) => {
   return {
     type: "RECEIVE_CATEGORIES",
@@ -20,15 +21,16 @@ const receiveCategories = (categories) => {
 
 const receiveCategoryProducts = (products) => {
   return {
-    type: "RECIEVE_CATEGORY_PRODUCTS",
-    products
-  }
+    type: "RECEIVE_CATEGORY_PRODUCTS",
+    products,
+  };
 }
 
 // create thunk action creators involving dispatch
 // will be exporting these functions to be used in frontend, similar to util
 // return api request from api util
 // which then returns promise simlar top action thunk creator
+// #1
 export const listAllCategories = () => (dispatch) => {
   return CategoryApiUtil.fetchAllCategories()
     .then((categories) => { return dispatch(receiveCategories(categories))}) 
