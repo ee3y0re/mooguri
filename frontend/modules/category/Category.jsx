9376025fbbf0ad8a8f50e218360d673d335f50c1
ajoyvmon <ajoyvmon@gmail.com>
2023-02-14 21:17:42 -0800

    update packages and fetch category products

--- frontend/modules/category/Category.jsx
+++ frontend/modules/category/Category.jsx
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { listCategoryProducts } from "../../actions/category_actions";
 
-const Category = () => {
+const Category = (props) => {
+  useEffect(() => {
+    dispatch(listCategoryProducts(props.match.params.categoryId));
+  }, []);
+
+  const categoryResponse = useSelector((state) => state.entities.categories);
+  const dispatch = useDispatch();
+  const category = {
+    id: categoryResponse && categoryResponse.id,
+    name: categoryResponse && categoryResponse.name,
+  };
+  const products = categoryResponse && categoryResponse.products;
+
   return (
     <div>
-      <h1>Home & Living</h1>
+      <h1>{category.name}</h1>
       <h2>Find something you love</h2>
       <ul>
         <li>
-          <img src="" alt="product 1 img" />
+          <img
+            src={placeholder}
+            alt="placeholder image"
+            style={{
+              width: "100%",
+              maxWidth: "300px",
+              maxHeight: "300px",
+              objectFit: "cover",
+            }}
+          />
           <h3>product title</h3>
           <p>product price</p>
           <p>product seller</p>
