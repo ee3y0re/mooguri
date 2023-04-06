b2fcb35ee18a93a5ef0bf275a0a6fa6f0e0f05c9
ajoyvmon <ajoyvmon@gmail.com>
2023-04-06 11:58:09 -0700

    style styling product list by resizing photos

--- frontend/components/product/list/ProductList.jsx
+++ frontend/components/product/list/ProductList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Container from "../../../modules/container/Container";
+import './ProductList.scss'
 
 class ProductList extends React.Component {
   componentDidMount() {
@@ -22,37 +23,6 @@ class ProductList extends React.Component {
     let firstAndSecond = firstSet.concat(secondSet);
 
     return (
-      // <div className="section-product-index">
-      //   <div className="product-index-grid-container">
-      //     <div className="product-index-row">
-      //       <h1 className="product-index-title">See MOO-re</h1>
-      //       <ul className="ul-product-index-row-list">
-      //         {
-      //           firstAndSecond?.map((product) => {
-      //             return (
-      //               <div
-      //                 className="product-index-row-list-items-container"
-      //                 key={product.id.toString()}
-      //               >
-      //                 <li className="product-index-row-list-items">
-      //                   <Link
-      //                     to={`/products/${product.id}`}
-      //                   >
-      //                     <img
-      //                       src={product.photoUrl}
-      //                       alt={`${product.name}`}
-      //                       className="product-index-thumbnail"
-      //                     />
-      //                   </Link>
-      //                 </li>
-      //               </div>
-      //             )
-      //           })
-      //         }
-      //       </ul>
-      //     </div>
-      //   </div>
-      // </div>
       <Container className="sample-container">
         <h3 className="sample-title">See Moo-re</h3>
         <ul className="sample-list">
